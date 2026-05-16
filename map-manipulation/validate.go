4e0e774848b88d483e0982fb8efa9dfdf0fc4616
banebi <blessingoyiwodu2020@gmail.com>
2026-05-16 15:46:03 +0100

    today

--- map-manipulation/validate.go
+++ map-manipulation/validate.go
@@ -1,27 +1,38 @@
 package main
+
 import "fmt"
+
 func ValidateBanner(banner map[rune][]string) error {
-	if banner == nil{
-		return fmt.errorf("banner is nil", err)
+
+	if banner == nil {
+		return fmt.Errorf("banner is nil")
 	}
-	if len(banner) != 95{
-		return fmt.errorf("incomplete")
+
+	if len(banner) != 95 {
+		return fmt.Errorf("incomplete")
 	}
-	for r := 32; r <= 126; r++{
+
+	for r := rune(32); r <= 126; r++ {
+
 		lines, ok := banner[r]
-		if !ok{
-return fmt.errorf("invalid")
+
+		if !ok {
+			return fmt.Errorf("invalid")
 		}
-		if len(lines) != 8{
-		return	fmt.errof("incomplete rows")
+
+		if len(lines) != 8 {
+			return fmt.Errorf("incomplete rows")
 		}
 	}
+
+	return nil
 }
+
 func main() {
 
 	banner := map[rune][]string{
-		'A': {"line1","line2","line3","line4","line5","line6","line7","line8"},
-		'B': {"1","2","3","4","5","6","7","8"},
+		'A': {"line1", "line2", "line3", "line4", "line5", "line6", "line7", "line8"},
+		'B': {"1", "2", "3", "4", "5", "6", "7", "8"},
 	}
 
 	err := ValidateBanner(banner)
